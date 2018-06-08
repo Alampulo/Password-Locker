630ab73bb143695e4f1677cb3fb17f521f2920e0
Blankphrase <ckasera6486@gmail.com>
2018-06-08 11:10:36 +0300

    creation of  class UserData

---
+++ userData.py
@@ -0,0 +1,13 @@
+class UserData:
+    """
+    """
+
+    def __init__(self, firstName, lastName, email, password, confirmPassword):
+        
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = password
+        self.confirmPassword = confirmPassword
+
+    
