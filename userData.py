4acb77cd980616424e178bc72850d67b392505f8
Blankphrase <ckasera6486@gmail.com>
2018-06-10 11:02:02 +0300

    importing random and string modules for generating  password

--- userData.py
+++ userData.py
@@ -1,22 +1,62 @@
+import random
+import string
+
+global create_account
+
+
 class UserData:
-    """
-    """
+
+    '''
+    class to create new user accounts 
+    '''
 
     create_account = []
 
-    def __init__(self, firstName, lastName, email, username, password, confirmPassword):
-        
+    def __init__(self, firstName, lastName, email, username, nick_name, password, confirmPassword):
+
+        """
+        Initializes the class
+        """
+
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
         self.username = username
+        self.nick_name = nick_name
         self.password = password
         self.confirmPassword = confirmPassword
 
-        @classmethod
+    def save_account(self):
 
-    def save_account(cls):
+        """
+        saves the new user to create_account list
+        """
 
-        UserData.create_account.append(cls)
+        UserData.create_account.append(self)
 
     
+        @classmethod
+
+    def user_login(cls, username, password):
+
+        '''
+        authenticates the user 
+        '''
+        for user in UserData.create_account:
+            if user.username == username and user.password == password:
+                return user
+            return False
+
+    def randompassword():
+        chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+        size = random.randint(8, 12)
+        return ''.join(random.choice(chars) for x in range(size))
+    def generate_password():
+        '''
+        function to generate a new password
+        '''
+
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alphabet) for i in range(5))
+
+        return password
